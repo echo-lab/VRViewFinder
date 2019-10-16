f011bb10c848f8e179e01b256788df984ee3da90
Shuyi Sun <35696908+ssun83@users.noreply.github.com>
2019-10-16 14:05:53 -0700

    updated tester

---
+++ tester/Assets/SpawnCamera.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnCamera : MonoBehaviour
+{
+    public GameObject Camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            Instantiate(Camera, new Vector3(0, 1, -5), Quaternion.identity);
+            GameObject[] cameras = GameObject.FindGameObjectsWithTag("Camera");
+            int len = cameras.Length - 1;
+            KeyCode camKeyCode = (KeyCode)System.Enum.Parse(typeof(KeyCode), "Keypad" + len.ToString());
+            Debug.Log(camKeyCode);
+            Camera.GetComponent<SwitchCamera>().CameraNumber = camKeyCode;
+        }
+    }
+}
---
+++ tester/Assets/SwitchCamera.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchCamera : MonoBehaviour
+{
+    public KeyCode CameraNumber;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(CameraNumber))
+        {
+            GameObject[] cameras = GameObject.FindGameObjectsWithTag("Camera");
+            for (int i = 0; i < cameras.Length; i++)
+            {
+                cameras[i].GetComponent<Camera>().depth = -1;
+            }
+            GetComponent<Camera>().depth = 1;
+        }
+    }
+}
